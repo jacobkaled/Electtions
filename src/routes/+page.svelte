cf8ef7f5e953127675dd0b014c7fcd9800aedc4c
Jakub Alkhaled <jacobkaled@yahoo.com>
2024-04-06 22:58:11 +0200

    implement main seach page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,63 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import { goto } from "$app/navigation";
+  import SearchCombo from "$lib/components/SearchCombo.svelte";
+
+  type Regions = Array<{
+    okres: string;
+    obec: string;
+  }>;
+  let selectedRegion: { okres: string; obec: string } | null = null;
+
+  const getValue = (value: { okres: string; obec: string }) =>
+    (selectedRegion = value);
+
+  async function getCompanyDetails() {
+    const response = await fetch("/api/regions", {
+      method: "GET",
+      headers: {
+        "content-type": "application/json",
+      },
+    });
+    const data = (await response.json()) as Regions;
+    return data;
+  }
+</script>
+
+{#await getCompanyDetails()}
+  <p>...loading regions</p>
+{:then regions}
+  <main>
+    <nav>
+      <SearchCombo {getValue} options={regions} />
+      <button
+        on:click={() => {
+          const m = new URLSearchParams({ ...selectedRegion });
+          goto(`../results?${m}`);
+        }}>Results</button
+      >
+    </nav>
+  </main>
+{/await}
+
+<style>
+  main {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  nav {
+    display: flex;
+    gap: 10px;
+    height: 50px;
+  }
+  button {
+    all: unset;
+    padding: 5px 20px;
+    border-radius: 8px;
+    background-color: coral;
+    height: 100%;
+    color: white;
+  }
+</style>
